5597cacdd8a01f3367c43c7b42bf9666344519b7
Md Rakib Sarker <87212579+rakibsarker24@users.noreply.github.com>
2022-01-27 01:14:15 +0600

    Add files via upload
    JQuery, CounterUP plugins

---
+++ js/custom.js
@@ -0,0 +1,33 @@
+
+$(function () {
+
+
+  // $('.slider-main').slick({
+  //   slidesToShow: 1,
+  //   slidesToScroll: 1,
+  //   autoplay: true,
+  //   autoplaySpeed: 2000,
+  //   dots: true,
+  //   arrows: true,
+  //   prevArrow:'.left',
+  //   nextArrow: '.right',
+
+  // });
+      
+
+
+
+
+//Counter
+
+$('.counter').counterUp({
+  delay: 20,
+  time: 2000
+});
+
+
+
+
+
+
+});
